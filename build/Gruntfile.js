e6f2fe20d18f8db53827afa21c640bd2490d4990
domovoj <yura.levantovych@gmail.com>
2015-04-20 15:24:05 +0300

    add build

---
+++ build/Gruntfile.js
@@ -0,0 +1,48 @@
+/*
+ * jScrollPane build script
+ * http://jscrollpane.kelvinluck.com/
+ *
+ * Copyright (c) 2013 Kelvin Luck
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        uglify: {
+            pm: {
+                files: {
+                    '../plusminus.min.js': '../plusminus.js'
+                },
+                options: {
+                    preserveComments: 'some'
+                }
+            }
+        },
+        watch: {
+            content: {
+                files: ['../plusminus.js'],
+                tasks: 'uglify'
+            }
+        },
+        connect: {
+            site: {
+                options: {
+                    base: '../'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('serve', ['uglify', 'connect', 'watch']);
+
+};
